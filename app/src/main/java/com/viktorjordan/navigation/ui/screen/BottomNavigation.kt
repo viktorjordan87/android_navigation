69806ea28f71a4c55634b25bf97bc490db64fa0e
Viktor Jordan <jordangergoviktor@gmail.com>
2024-08-03 22:27:01 +0200

    init

---
+++ app/src/main/java/com/viktorjordan/navigation/ui/screen/BottomNavigation.kt
@@ -0,0 +1,125 @@
+package com.viktorjordan.navigation.ui.screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+
+data class BottomNavigationItem(
+	val title: String,
+	val route: String,
+	val selectedIcon: ImageVector,
+	val unselectedIcon: ImageVector,
+	val hasNews: Boolean = false,
+	val badgeCount: Int? = null
+)
+
+
+@Composable
+fun BottomNavScreen() {
+	//List of Bottom Navigation Items
+	val items = listOf(
+		BottomNavigationItem(
+			title = "Home",
+			route = "home",
+			selectedIcon = Icons.Filled.Home,
+			unselectedIcon = Icons.Outlined.Home,
+			badgeCount = 5
+		),
+		BottomNavigationItem(
+			title = "Search",
+			route = "search",
+			selectedIcon = Icons.Filled.Search,
+			unselectedIcon = Icons.Outlined.Search,
+			hasNews = true,
+		),
+		BottomNavigationItem(
+			title = "Profile",
+			route = "profile",
+			selectedIcon = Icons.Filled.Person,
+			unselectedIcon = Icons.Outlined.Person
+		)
+	)
+	//Add a state to hold the selected item index
+	var selectedItemIndex by rememberSaveable {
+		mutableStateOf(0)
+	}
+	
+	
+	Scaffold(
+		bottomBar = {
+			// Bottom Navigation
+			NavigationBar {
+				//loop through the items
+				items.forEachIndexed() { index, item ->
+					// Bottom Navigation Item
+					NavigationBarItem(
+						selected = selectedItemIndex == index,
+						onClick = {
+							selectedItemIndex = index
+						},
+						alwaysShowLabel = true,
+						label = {
+							Text(text = item.title)
+						},
+						icon = {
+							BadgedBox(
+								badge = {
+									//if statment
+									if (item.badgeCount != null) {
+										Badge {
+											Text(text = item.badgeCount.toString())
+										}
+									} else if (item.hasNews) {
+										Badge(
+											modifier = Modifier.size(8.dp) // Adjust the size as needed
+										) {
+											Text(text = "•")
+										}
+									}
+									//end of if statement
+								}
+							) {
+								Icon(
+									imageVector = if (selectedItemIndex == index) {
+										item.selectedIcon
+									} else {
+										item.unselectedIcon
+									},
+									contentDescription = item.title
+								)
+							}
+						},
+						
+						)
+				}
+			}
+		}
+	) {
+		// Screen content
+		Text(text = "Screen content", modifier = Modifier.padding(it))
+	}
+}
+	
+
